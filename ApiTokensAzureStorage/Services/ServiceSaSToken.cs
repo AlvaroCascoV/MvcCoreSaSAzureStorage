7a6199c06099970290da046feffd564b6f714838
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-04-14 13:27:45 +0200

    minimal api funcionando para conseguir tokens con filtro

---
+++ ApiTokensAzureStorage/Services/ServiceSaSToken.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Data.Tables;
+using Azure.Data.Tables.Sas;
+
+namespace ApiTokensAzureStorage.Services
+{
+    public class ServiceSaSToken
+    {
+        private TableClient tableAlumnos;
+        public ServiceSaSToken(IConfiguration configuration)
+        {
+            string azureKeys = configuration.GetValue<string>("AzureKeys:StorageAccount");
+            TableServiceClient tableService = new TableServiceClient(azureKeys);
+            this.tableAlumnos = tableService.GetTableClient("alumnos");
+        }
+
+        //se pueden generar tokens generales o con filtros
+        public string GenerateToken(string curso)
+        {
+            //NECESITAMOS LOS PERMISOS DE ACCESO
+            TableSasPermissions permisos = TableSasPermissions.Read; //para mas permisos "| TableSasPermissions.Add | TableSasPermissions.Update | TableSasPermissions.Delete";
+            //EL ACCESO A TOKEN ESTA DELIMITADO POR UN TIEMPO DETERMINADO
+            TableSasBuilder builder = this.tableAlumnos.GetSasBuilder(permisos, DateTimeOffset.UtcNow.AddMinutes(30));
+            //EL ACCESO A LOS DATOS ES MEDIANTE ROWKEY O
+            //PARTITIONKEY. SON STRING Y VAN DE FORMA
+            //ALFABETICA
+            builder.PartitionKeyStart = curso;
+            builder.PartitionKeyEnd = curso;
+            //YA TENDREMOS EL TOKEN QUE ES UN ACCESO MEDIANTE URI
+            Uri uriToken = this.tableAlumnos.GenerateSasUri(builder);
+            string token = uriToken.AbsoluteUri;
+            return token;
+        }
+    }
+}
